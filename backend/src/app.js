31154ad8a0f58a0a21f8717df91eb62cb0b45077
Steven Star <stevenstar5500@gmail.com>
2026-02-22 05:29:30 +0530

    Added Database and User Model

---
+++ backend/src/app.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
